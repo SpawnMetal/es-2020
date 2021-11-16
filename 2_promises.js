1bde73fb9a501a3bec8de1259c6ed63774fe5f55
SpawnMetal <46967498+SpawnMetal@users.noreply.github.com>
2021-11-16 14:19:45 +0700

    Updated 2021.11.16

--- 2_promises.js
+++ 2_promises.js
@@ -8,8 +8,15 @@ const p3 = Promise.resolve(3)
 //     console.log(result)
 // })()
 
-;(async() => //Исключительная ситуация, где нужна ;
-{
-    const result = await Promise.allSettled([p1, p2, p3]) //allSetteld Не попадёт в catch
-    console.log(result)
+//Исключительная ситуация, где нужна ;
+;(async () => {
+  const result = await Promise.allSettled([p1, p2, p3]) //allSetteld Не попадёт в catch
+  console.log(result)
 })()
+
+new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('qwe')
+    console.log('setTimeout')
+  }, 2000)
+}).then(response => console.log('response =', response))
